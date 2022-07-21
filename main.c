944e1a8921fc194447d227e78a678a51fddb1541
Wouter Van de Wiele <wvandewiele@gmail.com>
2022-07-21 15:34:08 +0200

    testing out serial

--- main.c
+++ main.c
@@ -7,27 +7,38 @@
 #include "defs.h"
 #include "serial.h"
 
-#include <avr/sleep.h>
+// #include <avr/sleep.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+#include <util/delay_basic.h>
 
-#include <avr/sleep.h>
+// #include <avr/sleep.h>
 #include <avr/cpufunc.h>
 #include <stdio.h>
 
-// static int serial_putchar(char c, FILE *stream);
+static int serial_putchar(char c, FILE *stream);
 
-// static int serial_putchar(char c, FILE *stream)
-// {
-// 	if (c == '\n')
-// 		serial_putchar('\r', stream);
+static int serial_putchar(char c, FILE *stream)
+{
+	// SET_BIT(PORTB, TXPIN); // tx pin high, send 1
+	// _delay_loop_1(0);
+	// CLEAR_BIT(PORTB, TXPIN);
+	// _delay_loop_1(0);
+	if (c == '\n')
+		serial_putchar('\r', stream);
 	
-// 	transmit_byte(c);
+	transmit_byte(((u8)c));
 
-// 	return 0;
-// }
+	return 0;
+}
 
-// static FILE serial_output = FDEV_SETUP_STREAM(serial_putchar, NULL, _FDEV_SETUP_WRITE);
+static int serial_readchar();
+
+static int serial_readchar()
+{
+	return read_byte();
+}
+
+static FILE serial_stream = FDEV_SETUP_STREAM(serial_putchar, serial_readchar, _FDEV_SETUP_RW);
 
 // static void general_init()
 // {
@@ -69,15 +80,37 @@ int main()
 	cli();
 	serial_init();
 	sei();
+	stdout = &serial_stream;
+	stdin = &serial_stream;
+	stderr = &serial_stream;
 	while (1)
 	{
+		// serial loopback
 		read = read_byte();
 		transmit_byte(read);
 
+		// test();
+		// transmit_byte('s');
+		// transmit_byte('\n');
+		// transmit_byte('\r');
+		// transmit_byte('d');
+		// transmit_byte('\n');
+		// transmit_byte('\r');
+		// scanf("n%"PRIx8"\n", &read);
+		// scanf("\r");
+		// transmit_byte(read);
+		// printf("Hello world!\n");
+		// fprintf(stdout,"Hello world!\n");
+		// transmit_byte('f');
+		// transmit_byte('\n');
+		// transmit_byte('\r');
+		// SET_BIT(PORTB, TXPIN); // tx pin high, send 1
+		// _delay_loop_1(0);
+		// CLEAR_BIT(PORTB, TXPIN);
+		// _delay_loop_1(0);
 		// for (double j = 0; j<100000; j++);
 		// test();
 		// sprintf(stdout, "Hello world!\n");
-		// printf("Hello world!\n");
 		// fprintf(&serial_out, "%f\n", 3.1415);
 	}
 }
